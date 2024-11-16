0a659852688b8e534a002fcd44beae71a7b2827d
yuanqingqing <1194367511@qq.com>
2024-11-16 14:05:39 +0800

    fix 初始

---
+++ pages/device/device.js
@@ -0,0 +1,149 @@
+// pages/device.js
+const ecUI = require('../../utils/ecUI.js')
+const ecBLE = require('../../utils/ecBLE.js')
+
+let ctx
+let isCheckScroll = true
+let isCheckRevHex = false
+let isCheckSendHex = false
+let sendData = ''
+
+Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        textRevData: '',
+        scrollIntoView: 'scroll-view-bottom',
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad() {
+        ctx = this
+        isCheckScroll = true
+        isCheckRevHex = false
+        isCheckSendHex = false
+        sendData = ''
+        ecBLE.setChineseType(ecBLE.ECBLEChineseTypeGBK)
+
+        //on disconnect
+        ecBLE.onBLEConnectionStateChange(() => {
+            ecUI.showModal('提示', '设备断开连接')
+        })
+        //receive data
+        ecBLE.onBLECharacteristicValueChange((str, strHex) => {
+            let data =
+                ctx.data.textRevData +
+                ctx.dateFormat('[hh:mm:ss,S]:', new Date()) +
+                (isCheckRevHex ? strHex.replace(/[0-9a-fA-F]{2}/g, ' $&') : str) +
+                '\r\n'
+            // console.log(data)
+            ctx.setData({ textRevData: data })
+            if (isCheckScroll) {
+                if (ctx.data.scrollIntoView === "scroll-view-bottom") {
+                    ctx.setData({ scrollIntoView: "scroll-view-bottom2" })
+                } else {
+                    ctx.setData({ scrollIntoView: "scroll-view-bottom" })
+                }
+            }
+        })
+    },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+        ecBLE.onBLEConnectionStateChange(() => { })
+        ecBLE.onBLECharacteristicValueChange(() => { })
+        ecBLE.closeBLEConnection()
+    },
+    checkScroll(e) {
+        if (e.detail.value.length) isCheckScroll = true
+        else isCheckScroll = false
+    },
+    checkRevHex(e) {
+        if (e.detail.value.length) isCheckRevHex = true
+        else isCheckRevHex = false
+    },
+    checkSendHex(e) {
+        if (e.detail.value.length) isCheckSendHex = true
+        else isCheckSendHex = false
+    },
+    inputSendData(e) {
+        sendData = e.detail.value
+    },
+    btClearTap() {
+        this.setData({ textRevData: '' })
+    },
+    btSendTap() {
+        if (isCheckSendHex) {
+            let data = sendData
+                .replace(/\s*/g, '')
+                .replace(/\n/g, '')
+                .replace(/\r/g, '')
+            if (data.length === 0) {
+                ecUI.showModal('提示', '请输入要发送的数据')
+                return
+            }
+            if (data.length % 2 != 0) {
+                ecUI.showModal('提示', '数据长度只能是双数')
+                return
+            }
+            if (data.length > 488) {
+                ecUI.showModal('提示', '最多只能发送244字节')
+                return
+            }
+            if (!new RegExp('^[0-9a-fA-F]*$').test(data)) {
+                ecUI.showModal('提示', '数据格式错误，只能是0-9,a-f,A-F')
+                return
+            }
+            ecBLE.writeBLECharacteristicValue(data, true)
+        } else {
+            if (sendData.length === 0) {
+                ecUI.showModal('提示', '请输入要发送的数据')
+                return
+            }
+            let tempSendData = sendData.replace(/\n/g, '\r\n')
+            if (tempSendData.length > 244) {
+                ecUI.showModal('提示', '最多只能发送244字节')
+                return
+            }
+            ecBLE.writeBLECharacteristicValue(tempSendData, false)
+        }
+    },
+    dateFormat(fmt, date) {
+        let o = {
+            'M+': date.getMonth() + 1, //月份
+            'd+': date.getDate(), //日
+            'h+': date.getHours(), //小时
+            'm+': date.getMinutes(), //分
+            's+': date.getSeconds(), //秒
+            'q+': Math.floor((date.getMonth() + 3) / 3), //季度
+            S: date.getMilliseconds(), //毫秒
+        }
+        if (/(y+)/.test(fmt))
+            fmt = fmt.replace(
+                RegExp.$1,
+                (date.getFullYear() + '').substr(4 - RegExp.$1.length)
+            )
+        for (var k in o)
+            if (new RegExp('(' + k + ')').test(fmt)) {
+                // console.log(RegExp.$1.length)
+                // console.log(o[k])
+                fmt = fmt.replace(
+                    RegExp.$1,
+                    RegExp.$1.length == 1
+                        ? (o[k] + '').padStart(3, '0')
+                        : ('00' + o[k]).substr(('' + o[k]).length)
+                )
+            }
+        return fmt
+    },
+    checkChinese(e){
+        if(e.detail.value==='gbk'){
+            ecBLE.setChineseType(ecBLE.ECBLEChineseTypeGBK)
+        }else{
+            ecBLE.setChineseType(ecBLE.ECBLEChineseTypeUTF8)
+        }
+    }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,121 @@
+// index.js
+const ecUI = require('../../utils/ecUI.js')
+const ecBLE = require('../../utils/ecBLE.js')
+let ctx
+
+Page({
+    data: {
+        deviceListData: [],
+        deviceListDataShow: [],
+    },
+    onLoad() {
+        ctx = this
+        setInterval(() => {
+            ctx.setData({ deviceListDataShow: ctx.data.deviceListData })
+        }, 400)
+    },
+    onShow() {
+       
+    },
+    lanya(){
+      // wx.getUserInfo({
+      //   success(res) {
+      //     // const userInfo = res.userInfo;
+      //     console.log(res.userInfo);
+      //   },
+      //   fail() {
+      //     console.log('获取用户信息失败');
+      //   }
+      // });
+      console.log('点击了，123')
+      ctx.setData({ deviceListData: [] })
+      ctx.setData({ deviceListDataShow: [] })
+      setTimeout(() => {
+          ctx.openBluetoothAdapter()
+      }, 100)
+    },
+    bindGetUserInfo(event){
+      console.log('event',event.detail)
+    },
+    onPullDownRefresh: function () {
+        ctx.setData({ deviceListData: [] })
+        ctx.setData({ deviceListDataShow: [] })
+        setTimeout(() => {
+      console.log('setTimeout哈哈')
+            wx.stopPullDownRefresh()
+            ctx.openBluetoothAdapter()
+        }, 500)
+    },
+    listViewTap(event) {
+        ecUI.showLoading('设备连接中')
+        ecBLE.onBLEConnectionStateChange(res => {
+            ecUI.hideLoading()
+            if (res.ok) {
+                // ecBLE.stopBluetoothDevicesDiscovery()
+                wx.navigateTo({ url: '../device/device' })
+            } else {
+                ecUI.showModal(
+                    '提示',
+                    '连接失败,errCode=' + res.errCode + ',errMsg=' + res.errMsg
+                )  
+            }    
+        })  
+        const device = event.currentTarget.dataset.name
+        ecBLE.createBLEConnection(device.id)
+    },
+    openBluetoothAdapter() {
+      console.log('index.js,openBluetoothAdapter')
+        ecBLE.onBluetoothAdapterStateChange(res => {
+          console.log('什么呀呀呀呀呀呀',res)
+            if (res.ok) {
+                console.log('Bluetooth adapter ok')
+                ctx.startBluetoothDevicesDiscovery()
+            } else {
+                ecUI.showModal(
+                    '提示',
+                    `Bluetooth adapter error | ${res.errCode} | ${res.errMsg}`,
+                    () => {
+                        if (res.errCode === 30001) {
+                            wx.openSystemBluetoothSetting()
+                        }
+                        if (res.errCode === 30003) {
+                            wx.openAppAuthorizeSetting()
+                        }
+                        if (res.errCode === 30004) {
+                            //跳转到小程序设置界面
+                            wx.openSetting()
+                        }
+                    }
+                )
+            }
+        })
+        ecBLE.openBluetoothAdapter()
+    },
+    startBluetoothDevicesDiscovery() {
+        console.log('start search')
+        ecBLE.onBluetoothDeviceFound(res => {
+            // console.log(`id:${res.id},name:${res.name},rssi:${res.rssi}`)
+            for (const item of ctx.data.deviceListData) {
+                if (item.id === res.id) {
+                    item.name = res.name
+                    item.rssi = res.rssi
+                    return
+                }
+            }
+            let manufacturer = ''
+            if (res.name.length === 11 && res.name.startsWith('@')) {
+                manufacturer = 'eciot'
+            }
+            if (res.name.length === 15 && res.name.startsWith('BT_')) {
+                manufacturer = 'eciot'
+            }
+            ctx.data.deviceListData.push({
+                id: res.id,
+                name: res.name,
+                rssi: res.rssi,
+                manufacturer,
+            })
+        })
+        ecBLE.startBluetoothDevicesDiscovery()
+    },
+})
